02f48f32977c10e3ffdcb1c3037ab5fcca91a8ca
Jirawin Jitpakdee <Jirawin Jitpakdee>
2026-04-23 19:40:12 +0700

    multiple value on c programming

--- app.c
+++ app.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 int main(){
 
-
-
 int number;
 printf("Enter the number: ");
 scanf("%d", &number);
 
 int count = 1;
-while(count < 10){
+while(count < 5){
     int product = number * count;
-    printf("%d\n", product);
+    printf("%d*%d = %d\n",number,count, product);
     count = count + 1;
+
 }
-return 0;
 }
