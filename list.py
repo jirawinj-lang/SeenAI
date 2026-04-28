f1a4837ba50bd19c40ffaa28b20b7cde8919cee8
Jirawin Jitpakdee <Jirawin Jitpakdee>
2026-04-28 21:20:58 +0700

    list.py

---
+++ list.py
@@ -0,0 +1,14 @@
+course = 1
+while course < 10:
+    print(course)
+    course += 1
+
+    numbers = [3,4,55,89,5]
+    max = numbers[1]
+    for number in numbers:
+        if number > max:
+            max = number
+    print(max)
+
+
+    
