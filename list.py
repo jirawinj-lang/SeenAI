40023f7e1056cf485959238a70a9666ae3376e86
Jirawin Jitpakdee <Jirawin Jitpakdee>
2026-05-01 13:39:08 +0700

    find the maximun vlaue

--- list.py
+++ list.py
@@ -1,14 +1,9 @@
-course = 1
-while course < 10:
-    print(course)
-    course += 1
-
-    numbers = [3,4,55,89,5]
-    max = numbers[1]
-    for number in numbers:
-        if number > max:
-            max = number
-    print(max)
+numbers = [2,4,5,6,77,4]
+max = numbers[0]
+for number in numbers:
+    if number > max:
+        max = number
+print(max)
 
 
-    
+
