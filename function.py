e7082cb305ed304962c31bfc6fd25c66c3e01c04
Jirawin Jitpakdee <Jirawin Jitpakdee>
2026-05-04 11:46:06 +0700

    parameters.py

--- function.py
+++ function.py
@@ -1,11 +1,9 @@
-def greet_user(name):
-    print(f"Hi {name}!")
-    print("Wellcome aboard")
+def greet_user(first_name,last_name):
+    print(f"Hi {first_name} {last_name}!")
+    print("Welcome aboard")
 
 
-print("start")
-greet_user("Seen")
-greet_user("Mary")
-print("Finish")
-
-
+print("Start")
+greet_user("Seen", "Jitpakdee")
+greet_user("Sam","altman")
+print("The end")
