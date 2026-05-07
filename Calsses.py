b0fa01b8c87050c13c3bac5c60dedd893c89dc7f
Jirawin Jitpakdee <Jirawin Jitpakdee>
2026-05-07 09:41:10 +0700

    classes.py

---
+++ Calsses.py
@@ -0,0 +1,16 @@
+class Point:
+    def move(self):
+        print("move")
+
+    def draw(self):
+        print("draw")
+
+point1 = Point()
+point1.x = 10
+point1.y = 2
+print(point1.y)
+point1.draw()
+
+point2 = Point()
+point2.x = 2
+print(point2.x)
