aad7a26502085b75a5b9d5da201f486822279603
Jirawin Jitpakdee <Jirawin Jitpakdee>
2026-05-12 11:07:33 +0700

    modules.py

--- app.py
+++ app.py
@@ -1,31 +1,5 @@
-secret_number = 9
-guess_count = 0
-guess_limit = 3
-while guess_count < guess_limit:
-    guess = int(input('Guess: '))
-    guess_count += 1
-    if guess == secret_number:
-        print("You woun!")
-        break
-    else:
-        print("You failed!!!")
+from utils import find_max
 
-    weight = int(input('Weight: '))
-    unit = input('(L)bs or (K)g: ')
-    if unit.upper() == "L":
-        converted = weight * 0.45
-        print(f"you are {converted} kilos")
-    else:
-        converted = weight / 0.45
-        print(f"you are {converted} pounds")
-
-
-        is_hot = True
-        is_cold = False
-        if is_hot:
-            print("it's a hot day")
-            print("get some drinks")
-        elif is_cold:
-            print("it's a cold day")
-            print("wear war clothes")
-        print("enjoy our day:) ")
+numbers = [10, 3, 6, 2]
+# max = find_max(numbers)
+print(max(numbers))
