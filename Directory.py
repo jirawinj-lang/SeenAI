b952db23197ce5ea38c30e210bdfda084c96f368
Jirawin Jitpakdee <Jirawin Jitpakdee>
2026-05-19 11:02:56 +0700

    to show all file and folder in vscode

--- Directory.py
+++ Directory.py
@@ -1,5 +1,13 @@
+# show all file in that directory
+from pathlib import Path
+path = Path()
+for file in path.glob()
+
+
+#show folder and file in that directory
+
 from pathlib import Path
 
 path = Path()
-for file in path.glob("*.py"):
+for file in path.glob("*"):
     print(file)
